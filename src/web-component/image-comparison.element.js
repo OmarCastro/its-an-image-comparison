58edff16e71fe66d3415ed5fcbe86d51f7410ff2
OmarCastro <omar.castro.360@gmail.com>
2024-08-11 16:34:47 +0100

    src: setup entrypoint

--- src/web-component/image-comparison.element.js
+++ src/web-component/image-comparison.element.js
@@ -1,5 +1,12 @@
-import css from './qr-code.element.css'
+import html from './image-comparison.element.html'
+import css from './image-comparison.element.css'
 
+let loadTemplate = () => {
+  const templateElement = document.createElement('template')
+  templateElement.innerHTML = html
+  loadTemplate = () => templateElement
+  return templateElement
+}
 let loadStyles = () => {
   const sheet = new CSSStyleSheet()
   sheet.replaceSync(css)
@@ -10,5 +17,9 @@ let loadStyles = () => {
 export class ImageComparisonElement extends HTMLElement {
   constructor () {
     super()
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+    shadowRoot.adoptedStyleSheets = [loadStyles()]
+    const template = loadTemplate()
+    shadowRoot.append(document.importNode(template.content, true))
   }
 }
