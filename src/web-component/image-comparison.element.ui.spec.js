bbd51605bf1c204206ec4f53e95263aa066c9c10
OmarCastro <omar.castro.360@gmail.com>
2024-09-09 15:33:49 +0100

    test: add alpha compositiong UI tests

--- src/web-component/image-comparison.element.ui.spec.js
+++ src/web-component/image-comparison.element.ui.spec.js
@@ -1,8 +1,17 @@
 import { test } from '../../test-utils/ui/test.util.js'
 
-test('qr-code element visual test', async ({ page, expect }) => {
+test('image comparison base element visual test', async ({ page, expect }) => {
   await page.goto('./build/docs/test-page.html');
   const qrCode = page.locator('.image-comparison-1')
   await expect.soft(await qrCode.screenshot()).toMatchSnapshot('image-comparison--hello-world.png');
 });
 
+
+
+test('image comparison transparent image element visual test', async ({ page, expect }) => {
+  await page.goto('./build/docs/test-page.html');
+  const qrCode = page.locator('.image-comparison-2')
+  await expect.soft(await qrCode.screenshot()).toMatchSnapshot('image-comparison--alpha-light.png');
+  await page.emulateMedia({ colorScheme: 'dark' });
+  await expect.soft(await qrCode.screenshot()).toMatchSnapshot('image-comparison--alpha-dark.png');
+});
