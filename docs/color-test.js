a94fac81eda118e099d00266d6f933043d4f8ad9
OmarCastro <omar.castro.360@gmail.com>
2026-04-14 21:50:23 +0100

    docs: improve color test page by using tables

--- docs/color-test.js
+++ docs/color-test.js
@@ -1,5 +1,6 @@
-import { colorDelta } from '../src/utils/color-delta.js'
+import { colorDelta, validAlgorithms } from '../src/utils/color-delta.js'
 /** @import {rgbcolor} from '../src/utils/color-types.d.js' */
+/** @import {Algorithm} from '../src/utils/color-delta.js' */
 
 document.querySelectorAll('.rgb-test').forEach(testEl => {
   const block1 = testEl.querySelector(':scope > div.rgb-block-1')
@@ -10,16 +11,19 @@ document.querySelectorAll('.rgb-test').forEach(testEl => {
   console.log(rgb1)
   console.log(rgb2)
 
-  calculateDelta(rgb1, rgb2, 'YIQ', testEl.querySelector('.result-YIQ-b1-b2'))
-  calculateDelta(rgb2, rgb1, 'YIQ', testEl.querySelector('.result-YIQ-b2-b1'))
-  calculateDelta(rgb2, rgb1, 'CIEDE2000', testEl.querySelector('.result-CIEDE-b1-b2'))
-  calculateDelta(rgb2, rgb1, 'CIEDE2000', testEl.querySelector('.result-CIEDE-b2-b1'))
+  validAlgorithms.forEach(algorithm => {
+    calculateDelta(rgb1, rgb2, algorithm, testEl.querySelector('.result-YIQ-b1-b2'))
+  })
+
+  calculateDeltaTable(rgb1, rgb2, testEl.querySelector('.result-algoritms'))
 })
+
+
 
 /**
  * @param {rgbcolor} rgb1 - first target color
  * @param {rgbcolor} rgb2 - second target color
- * @param {Parameters<typeof colorDelta>[8]} algorithm - algorithm to use
+ * @param {Algorithm} algorithm - algorithm to use
  *  @param {Element} locationResult - location where to put the result
  */
 function calculateDelta (rgb1, rgb2, algorithm, locationResult) {
@@ -29,7 +33,45 @@ function calculateDelta (rgb1, rgb2, algorithm, locationResult) {
     ...delta,
     theshold: delta.delta / delta.maxDelta
   }
-  locationResult.textContent = JSON.stringify(result)
+  const resultText = `${algorithm} : ${JSON.stringify(result)}`
+  const p = document.createElement('p')
+  p.textContent = resultText
+  locationResult.append(p)
+}
+
+/**
+ * @param {rgbcolor} rgb1 - first target color
+ * @param {rgbcolor} rgb2 - second target color
+ * @param {Algorithm} algorithm - algorithm to use
+ *  @param {Element} locationResult - location where to put the result
+ */
+function calculateDeltaTable (rgb1, rgb2, locationResult) {
+  if (!locationResult) { return }
+
+  const results = validAlgorithms.map(algorithm => {
+    const delta = colorDelta(rgb1.r, rgb1.g, rgb1.b, 255, rgb2.r, rgb2.g, rgb2.b, 255, algorithm)
+    delta.delta = Math.abs(delta.delta)
+    const result = {
+      ...delta,
+      algorithm,
+      theshold: delta.delta / delta.maxDelta,
+    }
+    return result
+  })
+
+  const tableTemplate = document.querySelector("template.result-algorithms-table")
+  const rowTemplate = document.querySelector("template.result-algorithms-table-row")
+  const table = document.importNode(tableTemplate.content, true)
+  const tableTBody = table.querySelector("tbody")
+  results.forEach(result => {
+      const row = document.importNode(rowTemplate.content, true)
+      row.querySelector(".col-algorithm").textContent = result.algorithm
+      row.querySelector(".col-delta").textContent = result.delta
+      row.querySelector(".col-max-delta").textContent = result.maxDelta
+      row.querySelector(".col-theshold").textContent = result.theshold
+      tableTBody.append(row)
+  })
+  locationResult.append(table)
 }
 
 /**
