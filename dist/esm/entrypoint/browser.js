9e7234b3ea3de54f00bf44a75adc8a8c28182e78
OmarCastro <omar.castro.360@gmail.com>
2026-08-17 21:15:47 +0100

    release: cleanup

--- dist/esm/entrypoint/browser.js
+++
@@ -1,6 +0,0 @@
-import { ImageComparisonElement as Element } from '../web-component/image-comparison.element.js'
-const url = new URL(import.meta.url)
-const tagName = url.searchParams.get('named')?.trim()
-tagName && customElements.define(tagName, Element)
-export const ImageComparisonElement = Element
-export default ImageComparisonElement
--- dist/esm/entrypoint/node.js
+++
@@ -1,3 +0,0 @@
-import { ImageComparisonElement as Element } from '../web-component/image-comparison.element.js'
-export const ImageComparisonElement = Element
-export default ImageComparisonElement
